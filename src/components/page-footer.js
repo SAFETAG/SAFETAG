dadc13f783713a50f8567d9249a60f430e762d80
Jon Camfield <jon@openinternetproject.org>
2021-06-04 13:01:04 -0400

    Add matomo tracking code

--- src/components/page-footer.js
+++ src/components/page-footer.js
@@ -50,6 +50,23 @@ const GlobalFooter = () => {
           <a href="https://www.netlify.com">
               <img align="right" src="https://www.netlify.com/img/global/badges/netlify-light.svg" alt="Deploys by Netlify" />
             </a>
+          
+<!-- Matomo -->
+<script type="text/javascript">
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://stats.openinternetproject.org/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
+
         </PageNetlify>
       </PageFootInner>
     </PageFoot>
