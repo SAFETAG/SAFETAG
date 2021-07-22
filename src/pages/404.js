2cd20d37649323e1184bbec15c47191f16c148c6
Ricardo Lafuente <r@manufacturaindependente.org>
2021-07-22 15:54:41 +0100

    Format pages for translation
    Co-Authored-By: Neil Blazevic <blazman@users.noreply.github.com>

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,17 @@
 import React from "react"
-
+import {Trans, useTranslation} from 'gatsby-plugin-react-i18next';
 import GlobalLayout from "../components/layouts/global-layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
+const NotFoundPage = () => {
+  useTranslation('site', { useSuspense: false });
+  return (
   <GlobalLayout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1><Trans i18nKey="404-title">NOT FOUND</Trans></h1>
+    <p><Trans i18nKey="404-message">You just hit a route that doesn&#39;t exist... the sadness.</Trans></p>
   </GlobalLayout>
 )
+}
 
 export default NotFoundPage
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import {Trans, useTranslation} from 'gatsby-plugin-react-i18next';
 import GlobalLayout from "../components/layouts/global-layout"
 import SEO from "../components/seo"
 
@@ -12,29 +12,32 @@ import {
 
 import Heading from "../styles/type/heading"
 
-const AboutPage = () => (
+const AboutPage = () =>{
+  useTranslation('site', { useSuspense: false });
+  return (
   <GlobalLayout>
     <SEO title="About" />
       <Inpage>
         <InpageBody>
           <InpageBodyInner>
             <Heading size='jumbo' variation="primary" withDeco>
-              About Safetag
+              <Trans i18nKey="about-title">About Safetag</Trans>
             </Heading>
           </InpageBodyInner>
           <InpageInnerColumns columnLayout="2:1">
             <article>
-              <p>SAFETAG audits serve small scale civil society organizations and independent media houses who have digital security concerns by working with them to identify the risks they face and providing capacity-aware, pragmatic next steps to address them.</p>
+              <p><Trans i18nKey="about-text1">SAFETAG audits serve small scale civil society organizations and independent media houses who have digital security concerns by working with them to identify the risks they face and providing capacity-aware, pragmatic next steps to address them.</Trans></p>
 
-              <p>Traditional security audits are based upon the assumption that an organization has the time, money, and capacity to aim for perfect security. Low-income at-risk groups have none of these luxuries. SAFETAG combines assessment activities from the the security auditing world with best-practices for working with small scale at-risk organizations.</p>
+              <p><Trans i18nKey="about-text2">Traditional security audits are based upon the assumption that an organization has the time, money, and capacity to aim for perfect security. Low-income at-risk groups have none of these luxuries. SAFETAG combines assessment activities from the the security auditing world with best-practices for working with small scale at-risk organizations.</Trans></p>
 
-              <p>SAFETAG auditors lead a risk modeling process that helps staff and leadership take an institutional look at their digital security problems, expose vulnerabilities that impact their critical processes and assets, and provide clear reporting and follow up to help the organization strategically move forward and identify the support that they need.
+              <p><Trans i18nKey="about-text3">SAFETAG auditors lead a risk modeling process that helps staff and leadership take an institutional look at their digital security problems, expose vulnerabilities that impact their critical processes and assets, and provide clear reporting and follow up to help the organization strategically move forward and identify the support that they need.</Trans>
               </p>
             </article>
           </InpageInnerColumns>
         </InpageBody>
       </Inpage>
   </GlobalLayout>
-)
+  )
+}
 
 export default AboutPage
