b12c96457b88327603e97abc5da23ad99f008fac
Ricardo Lafuente <r@manufacturaindependente.org>
2021-09-28 09:59:34 +0100

    Quick commit to test builds

--- src/helpers/transifex.js
+++ src/helpers/transifex.js
@@ -33,10 +33,10 @@ function failed(err) {
 }
 
 function importFromTransifex(options) {
-  console.log(options.credential)
   // Retrieve all the data e.g. resource names, category names
   var transifex = new Transifex(options);
   transifex.resourcesSetMethod(options.project_slug, function(error, data) {
+    // console.log(data)
     if ( error ) {
       failed(error);
     }
@@ -59,8 +59,11 @@ function importFromTransifex(options) {
 
       // We are going to iterate through all the languages first before calling the function
       var wait = languages.teams.length;
+      // console.log(languages)
+      // console.log(languages.teams)
 
       // Check if there is more than one resource with the same category
+      /*
       var resources = data.filter(function(v) {
         if(v.categories !== null) {
           if(v.categories.indexOf(categoryName) !== -1) {
@@ -68,6 +71,8 @@ function importFromTransifex(options) {
           }
         }
       });
+      */
+      var resources = data
 
       if(!resources.length) {
         failed("Error: Please check your category name");
@@ -79,6 +84,7 @@ function importFromTransifex(options) {
           transifex.statisticsMethods(projectName, resource.slug, function(err, data) {
             // Write each file with the given filename and content.
             Object.keys(data).forEach(function(language){
+              console.log(language + resource)
               writeFile(path.join(language, "meta-" + resource.name + ".json"), JSON.stringify(data[language], null, 2), function( err ) {
                 if (err) {
                   throw new Error(err);
@@ -96,8 +102,10 @@ function importFromTransifex(options) {
                 throw new Error(err);
               }
               var filename = path.join(language, resource.name + "." + type);
+              filename = filename.replace(/_(activities|authors|guide_sections|methods|skills|references|approaches|infos|remote-options)_/, '/$1/')
               wait--;
               // Write each file with the given filename and content.
+              console.log(filename)
               writeFile(filename, fileContent, function( err ) {
                 if (err) {
                   throw new Error(err);
