f980099f4aee1ca20fc8499534f924a342a9bbe7
Ricardo Lafuente <r@manufacturaindependente.org>
2021-10-05 11:40:50 +0100

    First working version of search click-to-open link

--- src/components/search-box-header.js
+++ src/components/search-box-header.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { navigate } from 'gatsby';
 
 function SearchBox () {
   const [ searchQuery, setSearchQuery ] = useState('')
+  const [ isOpened, setIsOpened ] = useState(false)
+  const searchInput = useRef(null)
+
+  function showInputBox() {
+    setIsOpened(true)
+  }
 
   function handleSubmit(event) {
     event.preventDefault()
@@ -10,12 +16,23 @@ function SearchBox () {
   }
 
   return (
-      <form onSubmit={handleSubmit}>
-        <input type="text"
-          placeholder="Search..."
-          value={searchQuery ? searchQuery : ''}
-          onChange={() => setSearchQuery(event.target.value)}/>
-      </form>
+    <div className="searchBoxHeader">
+      <div className="boxTitle" onClick={showInputBox}
+          style={{ display: isOpened ? "none" : "block" }}>
+        Search
+      </div>
+
+      {isOpened && (
+        <form onSubmit={handleSubmit}>
+          <input type="text"
+            style={{ display: isOpened ? "block" : "none" }}
+            ref={searchInput}
+            placeholder="Search..."
+            value={searchQuery ? searchQuery : ''}
+            onChange={() => setSearchQuery(event.target.value)}/>
+        </form>
+      )}
+    </div>
   )
 }
 
