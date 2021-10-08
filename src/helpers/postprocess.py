100828dd7a6d9f690a47028d4b5e054311128b33
Ricardo Lafuente <r@manufacturaindependente.org>
2021-10-08 17:09:37 +0100

    Fix PDF Markdown rendering for methods

--- src/helpers/postprocess.py
+++ src/helpers/postprocess.py
@@ -16,9 +16,24 @@ def process_file(filename):
     newlines = []
     processing = False
     for line in lines:
-        if line.startswith(("walk_through: ", "recommendations: ", "overview: ")):
-            if line.strip().endswith(("|", "''")):
+
+        if line.startswith(
+            (
+                "walk_through: ",
+                "recommendations: ",
+                "overview: ",
+                "guiding_questions: ",
+                "operational_security: ",
+                "outputs: ",
+                "considerations: ",
+            )
+        ):
+            if line.strip().endswith(("|")):
                 newlines.append(line)
+                continue
+            elif line.strip().endswith(("''", '""')):
+                newlines.append(line)
+                processing = False
                 continue
             processing = True
             key, start = line.split(": ", 1)
@@ -35,6 +50,9 @@ def process_file(filename):
             newlines.append(line)
 
     output = "".join(newlines)
+    # replace double quotes with typographical quotes, mostly to not
+    # confuse the markdown renderer
+    output = output.replace('\\"', "”")
 
     f = open(filename, "w")
     f.write(output)
@@ -44,5 +62,6 @@ def process_file(filename):
 if __name__ == "__main__":
     # this will be run from the project root via npm, so that's the base path
     for f in glob.glob("locales/**/content/activities/*.md"):
-        # for f in glob.glob('locales/es/content/activities/*.md'):
+        process_file(f)
+    for f in glob.glob("locales/**/content/methods/*.md"):
         process_file(f)
