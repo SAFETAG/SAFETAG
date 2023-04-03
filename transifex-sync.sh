31bd789b5293c0887876edc2c5249c04a81d8467
Ricardo Lafuente <r@manufacturaindependente.org>
2023-04-03 10:48:54 +0100

    Add new Transifex helper script for its new API

---
+++ transifex-sync.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+
+function prepend() { while read line; do echo "${1}${line}"; done; }
+
+# per file
+MARKDOWN_FILES=$(find content/{activities,approaches,authors,guide_sections,infos,methods,references,skills,tools} -name '*.md')
+for FILEPATH in $MARKDOWN_FILES; do
+    # ./content/activities/example.md => content/activities/example.md
+    FILEPATH=$(echo $FILEPATH | sed 's/^\.\///')
+    # content/activities/example.md => activities_example
+    RESOURCE_SLUG=$(echo $FILEPATH | sed 's/\.md//' | tr '/' '_')
+    # content/activities/example.md => locales/<lang>/activities/example.md 
+    FILE_FILTER=$(echo $FILEPATH | prepend 'locales/<lang>/')
+
+    ./tx add \
+        --organization otf \
+        --project safetag-overview \
+        --resource $RESOURCE_SLUG \
+        --file-filter "$FILE_FILTER" \
+        --type GITHUBMARKDOWN \
+        $FILEPATH
+done
