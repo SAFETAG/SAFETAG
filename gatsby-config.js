954b3d55d7b22c8aac481f497bcd88d786981dea
Ricardo Lafuente <r@manufacturaindependente.org>
2023-04-06 09:47:55 +0100

    Enable French language

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,7 @@ module.exports = {
       resolve: `gatsby-plugin-react-i18next`,
       options: {
         localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
-        languages: [`en`, `my`],
+        languages: [`en`, `my`, `fr`],
         redirect: false,
         defaultLanguage: `en`,
         // if you are using Helmet, you must include siteUrl, and make sure you add http:https
